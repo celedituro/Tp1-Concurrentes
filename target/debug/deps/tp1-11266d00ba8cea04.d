3f0396e810d6a660b45ce179fdf80ae352f0c75b
celedituro <cdituro@fi.uba.ar>
2023-04-12 14:13:49 -0300

    refactor: run cargo clippy

---
+++ target/debug/deps/tp1-11266d00ba8cea04.d
@@ -0,0 +1,8 @@
+C:\Users\Asus\Desktop\Facultad\Técnicas de Programación Concurrente\2023-1c-tp1-celedituro\target\debug\deps\tp1-11266d00ba8cea04.rmeta: src\main.rs Cargo.toml
+
+C:\Users\Asus\Desktop\Facultad\Técnicas de Programación Concurrente\2023-1c-tp1-celedituro\target\debug\deps\tp1-11266d00ba8cea04.d: src\main.rs Cargo.toml
+
+src\main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
